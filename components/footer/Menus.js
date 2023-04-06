6ef5ad268600171111f4a3ec45db71ad8e2a84fa
Cute Face <84599947+aniekeme112@users.noreply.github.com>
2023-04-06 18:31:04 +0100

    Add files via upload

---
+++ components/footer/Menus.js
@@ -0,0 +1,27 @@
+export const PRODUCTS = [
+  { name: "MEDIA", link: "#" },
+  { name: "DONATE", link: "#" },
+  { name: "PROJECT", link: "#" },
+];
+export const RESOURCES = [
+  { name: "BLOG", link: "#" },
+  { name: "TEAM", link: "#" },
+  { name: "GALLERY", link: "#" },
+];
+export const COMPANY = [
+  { name: "WHO WE ARE", link: "#" },
+  { name: "ABOUT US", link: "#" },
+  { name: "CONTACT US", link: "#" },
+  { name: "ONLINE COMUNITIES", link: "#" },
+];
+export const SUPPORT = [
+  { name: "DONATE", link: "#" },
+  { name: "GET INVOLVE", link: "#" },
+];
+
+export const Icons = [
+  { name: "logo-facebook", link: "#" },
+  { name: "logo-twitter", link: "#" },
+  { name: "logo-youtube", link: "#" },
+  { name: "logo-instagram", link: "#" },
+];
